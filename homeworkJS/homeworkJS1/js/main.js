04890890d222de616b94c3612137f6f2433b1ff6
Atom1902 <atom1902@gmail.com>
2022-07-22 10:30:07 +0300

    new homework js

---
+++ homeworkJS/homeworkJS1/js/main.js
@@ -0,0 +1,62 @@
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+alert(b);
+
+let c = 'ua';
+document.write(c);
+
+let d = 1;
+console.log(d);
+
+let e = 10;
+alert(e);
+
+let f = -999;
+document.write(f);
+
+let g = 123;
+console.log(g);
+
+let h = 3.14;
+alert(h);
+
+let j = 2.7;
+document.write(j);
+
+let k = 16;
+console.log(k);
+
+let l = true;
+alert(l);
+
+let m = false;
+document.write(m);
+
+
+let firstName = 'Andri';
+let middleName = 'Stepanovich';
+let lastName = 'Bekerskiy';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let q = 100; 
+console.log(typeof q);
+
+let w = '100'; 
+console.log(typeof w);
+
+let r = true;
+console.log(typeof r);
+
+
+let inputDataFirstName = prompt('Name', 'Andri');
+console.log(inputDataFirstName);
+
+let inputDataMiddleName = prompt('MiddleName', 'Stepanovich');
+console.log(inputDataMiddleName);
+
+let inputDataYears= prompt('Years', 38);
+console.log(inputDataYears);
